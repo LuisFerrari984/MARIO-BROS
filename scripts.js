27a420d4dbe8379a08972d18cd8eb92559f73c7f
luisferrari984m <luisgustavoferrari18@gmail.com>
2026-01-08 11:08:45 -0300

    voltei o left para 50%

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const mascara = document.querySelector(".mascara-formulario")
 
 
 function CliqueiNoBotao() {
-    form.style.left = "20%"
+    form.style.left = "50%"
     form.style.transform = "translateX;(-50%)"
     mascara.style.visibility = "visible"
 
